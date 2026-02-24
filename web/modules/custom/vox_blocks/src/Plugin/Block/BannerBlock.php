27e04e1ba37abc771ceb2d403ac88d484a186755
dwisusanto <dwi.susanto@jejakin.com>
2026-02-25 01:00:41 +0700

    update banner and dump sql

--- web/modules/custom/vox_blocks/src/Plugin/Block/BannerBlock.php
+++ web/modules/custom/vox_blocks/src/Plugin/Block/BannerBlock.php
@@ -6,6 +6,7 @@ namespace Drupal\vox_blocks\Plugin\Block;
 
 use Drupal\Core\Block\Attribute\Block;
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\StringTranslation\TranslatableMarkup;
 
 /**
@@ -23,9 +24,48 @@ class BannerBlock extends BlockBase {
    */
   public function defaultConfiguration(): array {
     return [
-      'banner_title' => 'Welcome',
-      'banner_subtitle' => 'Stay updated with the latest events and news.',
+      'banner_title'    => 'Welcome to Vox Teneo',
+      'banner_subtitle' => 'We create effective solutions.',
+      'banner_image'    => '',
     ] + parent::defaultConfiguration();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockForm($form, FormStateInterface $form_state): array {
+    $form = parent::blockForm($form, $form_state);
+
+    $form['banner_title'] = [
+      '#type'          => 'textfield',
+      '#title'         => $this->t('Banner title'),
+      '#default_value' => $this->configuration['banner_title'],
+      '#required'      => TRUE,
+    ];
+
+    $form['banner_subtitle'] = [
+      '#type'          => 'textfield',
+      '#title'         => $this->t('Banner subtitle'),
+      '#default_value' => $this->configuration['banner_subtitle'],
+    ];
+
+    $form['banner_image'] = [
+      '#type'          => 'textfield',
+      '#title'         => $this->t('Background image URL'),
+      '#description'   => $this->t('Absolute URL to the background image, e.g. /sites/default/files/banner.jpg'),
+      '#default_value' => $this->configuration['banner_image'],
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockSubmit($form, FormStateInterface $form_state): void {
+    $this->configuration['banner_title']    = $form_state->getValue('banner_title');
+    $this->configuration['banner_subtitle'] = $form_state->getValue('banner_subtitle');
+    $this->configuration['banner_image']    = $form_state->getValue('banner_image');
   }
 
   /**
@@ -34,10 +74,13 @@ class BannerBlock extends BlockBase {
   public function build(): array {
     return [
       'banner_title' => [
-        '#markup' => $this->t($this->configuration['banner_title']),
+        '#markup' => $this->configuration['banner_title'],
       ],
       'banner_subtitle' => [
-        '#markup' => $this->t($this->configuration['banner_subtitle']),
+        '#markup' => $this->configuration['banner_subtitle'],
+      ],
+      'banner_image' => [
+        '#markup' => $this->configuration['banner_image'],
       ],
       '#cache' => [
         'max-age' => 3600,
